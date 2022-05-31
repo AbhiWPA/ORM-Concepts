4892505c0981a116e9b6c1d6ed4b15b1f65f54a4
WPAbhi <abhiashinsa770@gmail.com>
2022-05-31 09:13:49 +0530

    Basic Crud Operation Included

--- Hibernate-02/src/AppInitializer.java
+++ Hibernate-02/src/AppInitializer.java
@@ -14,16 +14,40 @@ public class AppInitializer {
         c1.setAddress("Panadura");
 
         Customer c2=new Customer();
-        c1.setId("C002");
-        c1.setName("Supun");
-        c1.setSalary(20000);
-        c1.setAddress("Colombo");
+        c2.setId("C002");
+        c2.setName("Supun");
+        c2.setSalary(20000);
+        c2.setAddress("Colombo");
 
         Session session = FactoryConfiguration.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
 
-        session.save(c1);
+        //for save
+        //session.save(c2);
+
+        //for Update
+        /*c1.setAddress("Galle");
+        session.update(c1);
+
+        c2.setSalary(45000);
+        session.update(c2);*/
+
+        //for get one customer
+        /*Customer c3 = session.get(Customer.class, "C001");
+        System.out.println(c1);
+        System.out.println(c3.getId());
+        System.out.println(c3.getName());
+        System.out.println(c3.getAddress());
+        System.out.println(c3.getSalary());*/
+
+        //for delete customer
+        Customer c3 = new Customer();
+            //catch the object first
+            c3.setId("C001");
+            //delete
+            session.delete(c3);
+
         transaction.commit();
 
         session.close();
